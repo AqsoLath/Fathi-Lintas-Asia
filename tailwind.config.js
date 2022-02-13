c9fd8aa47ec3e3fbea0874d974071ccda2cc59cf
Aqsha Lathif <axolath@gmail.com>
2022-02-14 00:47:42 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+            'ungu1': "#3d0066",
+            'ungu2': "#5c0099",
+            'ungu3': "#c86bfa",
+            'ungu4': "#9a01ce",
+            'emas1': "#fdc500",
+            'emas2': "#ffd500",
+            'emas3': "#ffee32",
+            'hitam': "#03071e"
+
+        },
+        fontFamily: {
+          roboto: "'roboto', sans-serif",
+          inter: "'inter', sans-serif",
+          akaya: "'Akaya Telivigala', cursive ",
+          poppins: "'Poppins', serif",
+          bree: "'Bree Serif', cursive", 
+        }
+    },
+  },
+  plugins: [],
+}
